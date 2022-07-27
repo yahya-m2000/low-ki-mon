7f0a7cc26743ce83f27d3a13badf1c0c3f60c159
SenpaiYG <yaxye_yssss@hotmail.com>
2022-07-27 12:07:21 +0100

    Changes

--- script.js
+++ script.js
@@ -10,18 +10,60 @@ console.log(c)
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, 
     canvas.height)
-// declare image variables that will contain image assests
+// declare image variables that will contain image assets
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
-
-const image = new Image() fdsfsdf
+ 
+const image = new Image() 
 image.src = './img/Map.png'
-image.onload = () => {
-    c.drawImage(image, -335, -690, 4000, 4000 * image.height / image.width)
+
+class Sprite {
+// Sprite is a variable that will give our sprites properties i.e the position of our sprite in the x & y axis
+    constructor({position, velocity, image}) {
+        this.position = position 
+        this.image = image 
+    }
+
+    draw() {
+        // drawImage will render the image variable (if you see on line 18 it contains the Map.png, so it'll render the background out)
+        c.drawImage(this.image, this.position.x, this.position.y )//(this.image, -335, -690, 4000, 4000 * image.height / image.width)
+    }
+}
+
+const background = new Sprite({position:{
+    x: -785,
+    y: -690, 
+    
+    },
+    // when Sprite runs, image: image will first run, then line 24 (this.image = image) and then line 27 (draw())
+    image: image
+})
+
+const keys = {
+    w: {
+        // key is not pressed by default, so it should evalute to false
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+
+
+}
+
+function animate () {
+window.requestAnimationFrame(animate) // This a recursive function and will loop through over and over again to begin editing the object properties 
+background.draw()
     c.drawImage(
         // this will crop the image so we don't have four sprites on screen
         playerImage,
-        // x co-ord
+        // x co-ord 
         0,
         // y co-ord
         0,
@@ -38,25 +80,62 @@ image.onload = () => {
         // height
         playerImage.height,
     )
+    
+    if (keys.w.pressed && lastKey === 'w') background.position.y += 3
+    else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
+    else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
+    else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3
+    
 }
+animate()
 
 // addEventListener('keydown') will work whenever you press a key
-
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
+let lastKey = ''
 window.addEventListener('keydown', (e) => {
     switch (e.key) {
-        case 'w':
-            console.log('pressed w key')
+
+        case 'w': // w will evaluate to true if it's pressed
+            keys.w.pressed = true
+            lastKey = 'w'
             break
+
         case 'a':
-            console.log('pressed a key')
+            keys.a.pressed = true
+            lastKey = 'a'
             break
+
         case 's':
-            console.log('pressed s key')
+            keys.s.pressed = true
+            lastKey = 's'
             break
+
         case 'd':
-            console.log('pressed d key')
+            keys.d.pressed = true
+            lastKey = 'd'
             break
     }
+    console.log(keys)
 })
 
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            // w will evaluate to false when you lift up off the key
+            keys.w.pressed = false
+            break
+
+        case 'a':
+            keys.a.pressed = false
+            break
+
+        case 's':
+            keys.s.pressed = false
+            break
+
+        case 'd':
+            keys.d.pressed = false
+            break
+    }
+    console.log(keys)
+})
